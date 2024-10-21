b8c248d97bd9903e62bb4b3b9ef4c09163288c21
Hugo <hoyola@ucv.edu.pe>
2024-10-21 09:22:32 -0500

    Añadir interfaces ObtenerDatosSedes, ObtenerDatosPlanes, Option, OptionGroup y Reclamo al archivo principal.ts

--- src/app/core/interface/responseResult.interface.ts
+++ src/app/core/interface/responseResult.interface.ts
@@ -26,3 +26,52 @@ export interface Pagination {
 	pageSize: number;
 	totalRows: number;
 }
+
+export interface Pagination {
+	pageIndex: number;
+	pageSize: number;
+	totalRows: number;
+}
+
+export interface DataSedes {
+	vcPerCodigo: string;
+	vnModuloId: number;
+	vnEsAutorizado: number;
+	vnTipoCurricula: number;
+	detalleConfiguracion: {
+		vIdModulo: number;
+		vnSisGruCodigo: number;
+		vnSisGruTipo: number;
+		vnObjTipo: number;
+		vnObjCodigo: string;
+	};
+}
+
+export interface DataPlanes {
+	vcPerCodigo: string;
+	vnModuloId: number;
+	vnEsAutorizado: number;
+	vnOpcionPadre: number;
+	vnPadreValor: number;
+	detalleConfiguracion: {
+		vIdModulo: number;
+		vnSisGruCodigo: number;
+		vnSisGruTipo: number;
+		vnObjTipo: number;
+		vnObjCodigo: string;
+	};
+}
+
+export interface ReclamoRequest {
+	cpercodigo: string;
+	cPerJuridica: string;
+	dFechaInicio: string;
+	dFechaFin: string;
+	cTipoReclamo?: string;
+	cEstadoReclamo?: string;
+	pagination: {
+		pageIndex: number;
+		pageSize: number;
+		totalRows: number;
+	};
+}
