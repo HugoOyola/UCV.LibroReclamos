f0b6f40f668349fdd869d870f26f500e7d711ee0
Hugo <hoyola@ucv.edu.pe>
2024-12-26 14:51:39 -0500

    Refactorizar el componente de búsqueda de reclamos, mejorando la legibilidad del código y la estructura de los tipos de datos emitidos

--- src/app/page/main/components/libro-reclamos/busqueda-reclamo/busqueda-reclamo.component.ts
+++ src/app/page/main/components/libro-reclamos/busqueda-reclamo/busqueda-reclamo.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, EventEmitter, Output, Input, ViewChild, OnChanges, SimpleChanges } from '@angular/core';
-import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, EventEmitter, Output, Input, ViewChild } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
@@ -11,14 +11,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
-import { Universidad } from '../../../interface/universidades';
-import { UniversidadService } from '../../../services/universidades.service';
-import { CCalendarComponent } from "../../../shared/c-calendar/c-calendar.component";
+import { CCalendarComponent } from '../../../shared/c-calendar/c-calendar.component';
 import { CSelectComponent } from '../../../shared/c-select/c-select.component';
-import { CRadioComponent } from "../../../shared/c-radio/c-radio.component";
+import { CRadioComponent } from '../../../shared/c-radio/c-radio.component';
 import { CInputComponent } from '../../../shared/c-input/c-input.component';
 import { CButtonComponent } from '../../../shared/c-button/c-button.component';
-
 
 @Component({
   selector: 'app-busqueda-reclamo',
@@ -41,14 +38,19 @@ import { CButtonComponent } from '../../../shared/c-button/c-button.component';
     CCalendarComponent,
     CRadioComponent,
     CInputComponent,
-    CButtonComponent
+    CButtonComponent,
   ],
   templateUrl: './busqueda-reclamo.component.html',
-  styleUrl: './busqueda-reclamo.component.scss'
+  styleUrl: './busqueda-reclamo.component.scss',
 })
 export class BusquedaReclamoComponent {
   @Input() public codigo: string = '';
-  @Output() public buscar = new EventEmitter<{ startDate: Date | null; endDate: Date | null; tipoReclamo: string; campus: string }>();
+  @Output() public buscar = new EventEmitter<{
+    startDate: Date | null;
+    endDate: Date | null;
+    tipoReclamo: string;
+    campus: string;
+  }>();
 
   @ViewChild(CCalendarComponent) public calendarComponent!: CCalendarComponent;
   @ViewChild(CRadioComponent) public radioComponent!: CRadioComponent;
@@ -59,7 +61,10 @@ export class BusquedaReclamoComponent {
   public tipoReclamo: string = '2';
 
   // Método para capturar el rango de fechas del componente calendario
-  onDateRangeSelected(range: { startDate: Date | null; endDate: Date | null }): void {
+  onDateRangeSelected(range: {
+    startDate: Date | null;
+    endDate: Date | null;
+  }): void {
     this.startDate = range.startDate;
     this.endDate = range.endDate;
   }
@@ -69,23 +74,23 @@ export class BusquedaReclamoComponent {
     this.tipoReclamo = tipo;
   }
 
-   // Método para capturar el campus seleccionado del componente select
+  // Método para capturar el campus seleccionado del componente select
   onCampusSelected(campus: string): void {
     this.campusSelected = campus; // Asignar el valor seleccionado a campusSelected
   }
 
   // Método que emite todos los valores cuando se hace clic en el botón de búsqueda
-   handleBusquedaDetallada(): void {
+  handleBusquedaDetallada(): void {
     this.buscar.emit({
       campus: this.campusSelected,
       startDate: this.startDate,
       endDate: this.endDate,
-      tipoReclamo: this.tipoReclamo
+      tipoReclamo: this.tipoReclamo,
     });
   }
 
   handleBusquedaCodigo(): void {
-    console.log("Buscar por Codigo");
+    console.log('Buscar por Codigo');
   }
 
   handleClear(): void {
